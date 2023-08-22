dec25e3380d5f712b4588ee8333bb2f1a438f4a3
Arron Evans <aredev45@gmail.com>
2023-08-22 12:31:14 +0000

    An array of 15 questions were created to iterate through with answer options and the correct answer to be called upon in the quiz.

---
+++ js/questions.js
@@ -0,0 +1,122 @@
+// An array containing objects, each representing a quiz question and its options
+const quizData = [
+    {question: "Sun Gate city of the Incas?", // Question text
+      a: "Cusco", // Option A text
+      b: "Machu Picchu", // Option B text
+      c: "Ollantaytambo", // Option C text
+      d: "Nazca", // Option D text
+      correct: "b", // Correct answer (a, b, c, or d)
+    },
+    {
+      question: "Viking-founded capital of Iceland?",
+      a: "Reykjavik",
+      b: "Oslo",
+      c: "Helsinki",
+      d: "Copenhagen",
+      correct: "a",
+    },
+    {
+      question: "Which of these is known as the City of Love?",
+      a: "Paris",
+      b: "Venice",
+      c: "Rome",
+      d: "Barcelona",
+      correct: "a",
+    },
+    {
+      question: "Official language of Brazil?",
+      a: "Spanish",
+      b: "Portuguese",
+      c: "French",
+      d: "Italian",
+      correct: "b",
+    },
+    {
+      question: "African wildlife safari country?",
+      a: "Kenya",
+      b: "South Africa",
+      c: "Egypt",
+      d: "Morocco",
+      correct: "a",
+    },
+    {
+      question: "Ancient Greek city?",
+      a: "Istanbul",
+      b: "Athens",
+      c: "Cairo",
+      d: "Rome",
+      correct: "b",
+    },
+    {
+      question: "Currency of Egypt?",
+      a: "Dollar",
+      b: "Pound",
+      c: "Riyal",
+      d: "Dinar",
+      correct: "b",
+    },
+    {
+      question: "Historic palace in India?",
+      a: "Hawa Mahal",
+      b: "Taj Mahal",
+      c: "Amber Palace",
+      d: "Fatehpur Sikri",
+      correct: "b",
+    },
+    {
+      question: "Land of the Pharaohs?",
+      a: "Greece",
+      b: "Turkey",
+      c: "Egypt",
+      d: "Italy",
+      correct: "c",
+    },
+    {
+      question: "Ancient city destroyed by volcano?",
+      a: "Pompeii",
+      b: "Herculaneum",
+      c: "Rome",
+      d: "Athens",
+      correct: "a",
+    },
+    {
+      question: "Which country is home to the Big Five safari animals?",
+      a: "Brazil",
+      b: "Kenya",
+      c: "China",
+      d: "Russia",
+      correct: "b",
+    },
+    {
+      question: "Amazon Rainforest country?",
+      a: "Colombia",
+      b: "Venezuela",
+      c: "Ecuador",
+      d: "Brazil",
+      correct: "d",
+    },
+    {
+      question: "Which desert is known as the Land of Fire?",
+      a: "Sahara",
+      b: "Atacama",
+      c: "Gobi",
+      d: "Kalahari",
+      correct: "b",
+    },
+    {
+      question: "Which of these is a US state?",
+      a: "California",
+      b: "Queensland",
+      c: "Lima",
+      d: "Quebec",
+      correct: "a",
+    },
+    {
+      question: "Capital city of Australia?",
+      a: "Sydney",
+      b: "Melbourne",
+      c: "Canberra",
+      d: "Brisbane",
+      correct: "c",
+    },
+  ];
