f8513651e61fdc5d29abe3620403a579da0cf298
Arron Evans <aredev45@gmail.com>
2023-08-22 12:37:54 +0000

    Created the functions to shuffle and load the quiz. Added functionality so the user can only select one answer. Also added functionality to the timer to begin when the quiz starts and a function to increment the question counter.

--- js/quiz.js
+++ js/quiz.js
@@ -13,3 +13,47 @@ const timerElement = document.getElementById("timer");
 let currentQuiz = 0;
 let score = 0;
 let timeLeft = 0;
+
+// Function to shuffle the order of elements in an array
+const shuffleArray = (array) => {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+  };
+  
+  // Function to clear the selection of answer choices
+  const deselectAnswers = () => {
+    answerElements.forEach((answer) => (answer.checked = false));
+  };
+  
+  // Function to get the ID of the selected answer choice
+  const getSelected = () => {
+    let answer;
+    answerElements.forEach((answerElement) => {
+      if (answerElement.checked) answer = answerElement.id;
+    });
+    return answer;
+  };
+  
+  // Function to load and display the current quiz question and answer choices
+  const loadQuiz = () => {
+    deselectAnswers();
+    const currentQuizData = quizData[currentQuiz];
+    questionElement.innerText = currentQuizData.question;
+    a_text.innerText = currentQuizData.a;
+    b_text.innerText = currentQuizData.b;
+    c_text.innerText = currentQuizData.c;
+    d_text.innerText = currentQuizData.d;
+  
+    // Counts what questions the user is on and increments by one
+    const questionCounterElement = document.getElementById("question-counter");
+    questionCounterElement.innerText = `${currentQuiz + 1} of ${quizData.length}`;
+  
+    // If it's the first question, set the initial time and start the timer
+    if (currentQuiz === 0) {
+      timeLeft = 60; // Time left for quiz (in seconds).
+      updateTimerDisplay();
+      startTimer();
+    }
+  };
